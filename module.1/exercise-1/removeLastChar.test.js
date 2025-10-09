ce38c68058333fc298041c37c137a5656e3aed74
Jake He <jake.he@nextgen.group>
2025-10-09 11:39:52 +1100

    more removeLastChar test

--- module.1/exercise-1/removeLastChar.test.js
+++ module.1/exercise-1/removeLastChar.test.js
@@ -3,6 +3,17 @@ import { describe, it, expect } from "vitest";
 
 describe("removeLastChar", () => {
   it("should remove the last character of a string", () => {
-    // expect(removeLastChar("Hello!")).toBe("Hello");
+    expect(removeLastChar("Hello!")).toBe("Hello");
   });
+
+  it("null should return null", () => {
+    expect(removeLastChar(null)).toBe(null);
+    // expect(removeLastChar("1")).toBe("");
+  });
+
+  // undefined should return undefined
+  // "" should return ""
+  // "a" should return ""
+  // 1.111 should return 1.111
+  // no agument should return undefined
 });
