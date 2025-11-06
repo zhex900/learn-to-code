a357179a1404b247bb12a6cbe8cb31aae4ce479e
Jake He <jake.he@nextgen.group>
2025-11-06 20:32:19 +1000

    swagger

--- module.5/calculator/index.js
+++ module.5/calculator/index.js
@@ -1,13 +1,17 @@
 // create express app to basic math functions
 import express from "express";
 import cors from "cors";
-import { add } from "./controller";
+import { add } from "./controller.js";
+
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./swagger.json" assert { type: "json" };
+
 const app = express();
 const port = 4000;
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(cors());
 
-//http://localhost:3000/add?a=1&b=20
 app.get("/add", add);
 
 app.listen(port, () => {
